c99174db682689d1405d73361fa387b74ee18789
Rick Twohy <twohyjr@gmail.com>
2018-02-05 09:23:20 -0700

    Initial Commit

---
+++ Banshee-Graphics/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Banshee-Graphics
+//
+//  Created by Rick Twohy on 2/5/18.
+//  Copyright © 2018 Flophouse-Atrocities. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
